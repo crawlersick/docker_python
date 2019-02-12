6d8b210f664f48a20bc453d4f8a819b03c43c6d6
john <johnbyyang@gmail.com>
2019-02-12 15:22:44 +0800

    add base url

--- docker_jupyterlab/build_docker.sh
+++ docker_jupyterlab/build_docker.sh
@@ -1,4 +1,6 @@
 cp -r ../docker_python0/instantclient_18_3 ./
+mkdir workingdir
+echo abc >> workingdir/testing.txt
 docker build -t jupyterlab .
 rm -r ./instantclient_18_3
 docker save jupyterlab |gzip -c > ~/docker_jupyterlab.tar.gz
