f70f4e14d3a3da044033360280bd8f915c9994e8
john <johnbyyang@gmail.com>
2019-02-13 11:13:36 +0800

    added good plotly suppot

--- docker_jupyterlab/build_docker.sh
+++ docker_jupyterlab/build_docker.sh
@@ -1,6 +1,7 @@
 cp -r ../docker_python0/instantclient_18_3 ./
 mkdir workingdir
 echo abc >> workingdir/testing.txt
-docker build -t jupyterlab .
+#docker build -t jupyterlab .
+docker build --network host --build-arg https_proxy=http://localhost:8998 --build-arg http_proxy=http://localhost:8998 -t jupyterlab .
 rm -r ./instantclient_18_3
 docker save jupyterlab |gzip -c > ~/docker_jupyterlab.tar.gz
