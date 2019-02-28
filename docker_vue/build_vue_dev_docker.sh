1efd57053a8182deb541478b5adceab18143cb3a
crawlersick <johnbyyang@gmail.com>
2019-02-28 22:06:04 +0800

    ,,

--- docker_vue/build_vue_dev_docker.sh
+++ docker_vue/build_vue_dev_docker.sh
@@ -1,1 +1,3 @@
-docker build --network host --build-arg https_proxy=http://localhost:8998 --build-arg http_proxy=http://localhost:8998 -t vuejsdev .
+#docker build --network host --build-arg https_proxy=http://localhost:8998 --build-arg http_proxy=http://localhost:8998 -t vuejsdev .
+docker build -t vuejsdev .
+docker save vuejsdev |gzip -c > $HOME/Downloads/docker_vue.tar.gz
