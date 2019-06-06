5009c0af0f624a2b13ebd88142030132001528bb
crawlersick <johnbyyang@gmail.com>
2019-06-06 10:54:36 +0800

    ,,

--- docker_python0/build_docker.sh
+++ docker_python0/build_docker.sh
@@ -1,2 +1,3 @@
 docker build -t py .
-docker save py |gzip -c > /home/sick/temp/docker/docker_py.tar.gz #~/Downloads/docker_py.tar.gz
+#docker build --build-arg http_proxy=http://192.168.8.100:8998 --build-arg https_proxy=http://192.168.8.100:8998 -t py .
+#docker save py |gzip -c > /home/sick/temp/docker/docker_py.tar.gz #~/Downloads/docker_py.tar.gz
