20011121a4bd29eab4bc424ce760b57cda57eda9
crawlersick <johnbyyang@gmail.com>
2019-09-03 19:09:14 +0800

    ..

--- docker_vue/module_build.sh
+++ docker_vue/module_build.sh
@@ -1,3 +1,6 @@
+sudo chmod -R 777 workingdir
+rm -rf workingdir/vue-admin-template/*
+cp package.json workingdir/vue-admin-template/
 docker run --name tempvue -it -v $PWD/workingdir/vue-admin-template:/app vuejsdev bash -c 'npm install; chmod -R 777 node_modules'
 docker rm tempvue
 
