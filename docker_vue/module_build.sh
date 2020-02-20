da24d1e4080d35148091e97be44f09e478b71b31
john <johnbyyang@gmail.com>
2020-02-20 15:30:11 +0800

    update

--- docker_vue/module_build.sh
+++ docker_vue/module_build.sh
@@ -1,6 +1,8 @@
-sudo chmod -R 777 workingdir
-rm -rf workingdir/vue-admin-template/*
-cp package.json workingdir/vue-admin-template/
-docker run --name tempvue -it -v $PWD/workingdir/vue-admin-template:/app vuejsdev bash -c 'npm install; chmod -R 777 node_modules'
 docker rm tempvue
-
+rm -rf workingdir/
+mkdir -p workingdir/vue-admin-template/
+sudo chmod -R 777 workingdir
+cp package.json workingdir/vue-admin-template/
+cp .npmrc workingdir/vue-admin-template/
+docker run --name tempvue -it -v $PWD/workingdir/vue-admin-template:/app vuejsdev bash -c 'node -v;ls -tral ; npm install; chmod -R 777 node_modules'
+docker rm tempvue
