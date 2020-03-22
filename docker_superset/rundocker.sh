887bba6e4ceb77c7a2ee1ef87bcda68b7b5a6df8
john <johnbyyang@gmail.com>
2020-03-22 23:06:30 +0800

    m

--- docker_superset/rundocker.sh
+++ docker_superset/rundocker.sh
@@ -1,2 +1,4 @@
-docker run --name mysuperset_c -it --network host superset
-docker cp  mysuperset_c:./root/.superset/superset.db ./
+docker rm -f mysuperset_c
+docker run --name mysuperset_c -it -p 10086:10086 -v $PWD/workingdir:/root/.superset/  -v $PWD/superset:/usr/local/lib/python3.6/site-packages/superset/ superset
+#docker cp  mysuperset_c:./root/.superset/superset.db ./
+#Public    [can explore on Superset, all database access on all_database_access, can explore json on Superset]
